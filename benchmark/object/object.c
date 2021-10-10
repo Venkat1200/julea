730cf308015f3aad48b7f7299eb6033dedfbd855
Venkat1200 <kvreddy1200@gmail.com>
2021-10-10 22:43:37 +0200

    Update object.c

--- benchmark/object/object.c
+++ benchmark/object/object.c
@@ -483,10 +483,143 @@ _benchmark_object_read(BenchmarkRun* run, gboolean use_batch, guint block_size)
 }
 
 static void
+_benchmark_object_workloadA(BenchmarkRun* run, gboolean use_batch, guint block_size)
+{
+	guint const n = (use_batch) ? 10000 : 1000;
+/**********************************/
+    gdouble latency;
+	guint perc;
+	double latencies[n];
+/**********************************/
+
+	g_autoptr(JObject) object = NULL;
+	g_autoptr(JBatch) batch = NULL;
+	g_autoptr(JSemantics) semantics = NULL;
+	gchar dummy[block_size];
+	guint64 nb = 0;
+	gboolean ret;
+
+	memset(dummy, 0, block_size);
+
+	semantics = j_benchmark_get_semantics();
+	batch = j_batch_new(semantics);
+
+	object = j_object_new("benchmark", "benchmark");
+	j_object_create(object, batch);
+
+
+	ret = j_batch_execute(batch);
+	g_assert_true(ret);
+	g_assert_cmpuint(nb, ==, n * block_size);
+
+	j_benchmark_timer_start(run);
+
+	while (j_benchmark_iterate(run))
+	{
+		for (guint i = 0; i < n; i++)
+		{
+			/**********************************/
+			g_autoptr(GTimer) func_timer = NULL;
+			func_timer = g_timer_new();
+                        g_timer_start(func_timer);
+			/**********************************/
+
+
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
+			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
+
+			if (!use_batch)
+			{
+				ret = j_batch_execute(batch);
+				g_assert_true(ret);
+				g_assert_cmpuint(nb, ==, block_size);
+			}
+			/**********************************/
+			
+			latency =1000000* g_timer_elapsed(func_timer, NULL);
+			latencies[i]=latency;
+                        if(run->min_latency < 0){
+                            run->min_latency=latency;
+                            run->max_latency=latency;
+
+                       }else{
+                            if(latency>run->max_latency)run->max_latency=latency;
+                            if(latency<run->min_latency)run->min_latency=latency;
+                        }
+			/**********************************/
+			
+			
+		}
+		
+		
+		/**********************************/
+		qsort(latencies, n, sizeof(double), compare);
+		perc=(int)((gdouble)0.95*(gdouble)n);
+		if(perc>=n)perc=n-1;
+		run->percLatnecy95=latencies[perc];
+		perc=(int)((gdouble)0.90*(gdouble)n);
+		if(perc>=n)perc=n-1;
+		run->percLatnecy90=latencies[perc];
+		//-/
+		run->latency=0;
+		for (guint iin = 0; iin < n; iin++)
+		run->latency=run->latency+latencies[iin];
+		run->latency=run->latency/n;
+		/**********************************/
+
+
+		if (use_batch)
+		{
+			ret = j_batch_execute(batch);
+			g_assert_true(ret);
+			g_assert_cmpuint(nb, ==, n * block_size);
+		}
+	}
+
+	j_benchmark_timer_stop(run);
+
+	j_object_delete(object, batch);
+	ret = j_batch_execute(batch);
+	g_assert_true(ret);
+
+	run->operations = n;
+	run->bytes = n * block_size;
+}
+static void
 benchmark_object_read(BenchmarkRun* run)
 {
 	_benchmark_object_read(run, FALSE, 4 * 1024);
 }
+
+
+static void
+benchmark_object_workloadA<(BenchmarkRun* run)
+{
+	_benchmark_object_workloadA(run, FALSE, 4 * 1024);
+}
+
 
 static void
 benchmark_object_read_batch(BenchmarkRun* run)
@@ -723,6 +856,11 @@ benchmark_object(void)
 	j_benchmark_add("/object/object/read-batch", benchmark_object_read_batch);
 	j_benchmark_add("/object/object/write", benchmark_object_write);
 	j_benchmark_add("/object/object/write-batch", benchmark_object_write_batch);
+	j_benchmark_add("/object/object/workload A(Scientific app)", benchmark_object_workloadA);
+	//j_benchmark_add("/object/object/workload B(Streaming)", benchmark_object_workloadB);
+	//j_benchmark_add("/object/object/workload C(AI)", benchmark_object_workloadC);
+	//j_benchmark_add("/object/object/workload F(Autonomous Sys)", benchmark_object_workloadF);
+
 	j_benchmark_add("/object/object/unordered-create-delete", benchmark_object_unordered_create_delete);
 	j_benchmark_add("/object/object/unordered-create-delete-batch", benchmark_object_unordered_create_delete_batch);
 }
