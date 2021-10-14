d9b9e0e6eac47136b3111ce6f74e0d6f58d7178f
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-10-14 20:14:35 +0200

    Update object.c

--- benchmark/object/object.c
+++ benchmark/object/object.c
@@ -537,7 +537,7 @@ _benchmark_object_workloadML(BenchmarkRun* run, gboolean use_batch, guint block_
 			for(int t=0;t<20;t++)
 			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
 			j_object_write(object, dummy, block_size, i * block_size, &nb, batch);
-			for(int t=0;t<5;t++)
+			for(int t=0;t<15;t++)
 			j_object_read(object, dummy, block_size, i * block_size, &nb, batch);
 
 			if (!use_batch)
