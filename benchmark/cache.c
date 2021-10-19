9d23c9badd50dc18057204509d543a7161b7ac81
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-10-19 20:39:54 +0200

    Update cache.c

--- benchmark/cache.c
+++ benchmark/cache.c
@@ -44,7 +44,7 @@ benchmark_cache_get_release(BenchmarkRun* run)
 /**********************************/
 	guint perc;
 	double latencies[n];
-    gdouble latency;
+    	gdouble latency;
 	
 /**********************************/
 
@@ -71,34 +71,34 @@ benchmark_cache_get_release(BenchmarkRun* run)
 			
 			/**********************************/
 			g_timer_stop(func_timer);
-			latency =1000000* g_timer_elapsed(func_timer, NULL);
-			latencies[i]=latency;
-                        if(run->min_latency < 0){
-                            run->min_latency=latency;
-                            run->max_latency=latency;
+			latency = 1000000* g_timer_elapsed(func_timer, NULL);
+			latencies[i] = latency;
+                        if (run->min_latency < 0){
+                            run->min_latency = latency;
+                            run->max_latency = latency;
 
-                       }else{
-                            if(latency>run->max_latency)run->max_latency=latency;
-                            if(latency<run->min_latency)run->min_latency=latency;
+                       }else {
+                            if (latency > run->max_latency)run->max_latency = latency;
+                            if (latency < run->min_latency)run->min_latency = latency;
                         }
 			/**********************************/
 			
 			
 		}
 		/**********************************/
-		 qsort(latencies, n, sizeof(double), compare);
-		perc=(int)((gdouble)0.95*(gdouble)n);
-		if(perc>=n)perc=n-1;
-		run->percLatnecy95=latencies[perc];
-		perc=(int)((gdouble)0.90*(gdouble)n);
-		if(perc>=n)perc=n-1;
-		run->percLatnecy90=latencies[perc];
+		qsort(latencies, n, sizeof(double), compare);
+		perc = (int)((gdouble)0.95*(gdouble)n);
+		if (perc>=n)perc = n-1;
+		run->percLatency95 = latencies[perc];
+		perc = (int)((gdouble)0.90*(gdouble)n);
+		if (perc>=n)perc = n-1;
+		run->percLatency90 = latencies[perc];
 		
 		//-/
-		run->latency=0;
+		run->latency = 0;
 		for (guint iin = 0; iin< n; iin++)
-		run->latency=run->latency+latencies[iin];
-		run->latency=run->latency/n;
+		run->latency = run->latency + latencies[iin];
+		run->latency = run->latency / n;
 		/**********************************/
 
 
