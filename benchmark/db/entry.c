7f421bdaf57cf93f720867f66161851f955e5d8a
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-10-19 23:20:43 +0200

    Update entry.c

--- benchmark/db/entry.c
+++ benchmark/db/entry.c
@@ -535,7 +535,7 @@ benchmark_db_update_batch_index_mixed(BenchmarkRun* run)
 void
 benchmark_db_entry(void)
 {
-	j_benchmark_add("/db/entry/workload 1(Scientific app)", benchmark_db_workloadScientific);
+
 	j_benchmark_add("/db/entry/insert", benchmark_db_insert);
 	j_benchmark_add("/db/entry/insert-batch", benchmark_db_insert_batch);
 	j_benchmark_add("/db/entry/insert-index-single", benchmark_db_insert_index_single);
@@ -560,6 +560,7 @@ benchmark_db_entry(void)
 	j_benchmark_add("/db/entry/update-batch-index-all", benchmark_db_update_batch_index_all);
 	j_benchmark_add("/db/entry/update-index-mixed", benchmark_db_update_index_mixed);
 	j_benchmark_add("/db/entry/update-batch-index-mixed", benchmark_db_update_batch_index_mixed);
+	j_benchmark_add("/db/entry/workload 1(Scientific app)", benchmark_db_workloadScientific);
 /*	j_benchmark_add("/db/entry/workload 2(Streaming)", benchmark_db_workloadStreaming);
 	j_benchmark_add("/db/entry/workload 3(Machine Learning)", benchmark_db_workloadML);
 	j_benchmark_add("/db/entry/workload 4(Autonomous Sys)", benchmark_db_workloadAutoSys);
