957bfe6f61bb15a83514c6fe571e2cb4d19b6bab
Venkat1200 <kvreddy1200@gmail.com>
2021-10-23 06:48:47 +0200

    Update common.c

--- benchmark/db/common.c
+++ benchmark/db/common.c
@@ -299,18 +299,30 @@ _benchmark_db_workload_insert(BenchmarkRun* run, JDBSchema* scheme, gchar const*
 	g_autoptr(GError) b_s_error = NULL;
 	g_autoptr(JDBSchema) b_scheme = NULL;
 
-/*	semantics = j_benchmark_get_semantics();
+	semantics = j_benchmark_get_semantics();
 	delete_batch = j_batch_new(semantics);
 	batch = j_batch_new(semantics);
+	if (use_timer)
+	{
+		g_assert_null(scheme);
+		g_assert_nonnull(run);
+
+		b_scheme = _benchmark_db_prepare_scheme(namespace, use_batch, use_index_all, use_index_single, batch, delete_batch);
+		g_assert_nonnull(b_scheme);
+
+	}
+	else
+	{
 		g_assert_true(use_batch);
 		g_assert_null(run);
 		g_assert_nonnull(scheme);
 
 		j_db_schema_ref(scheme);
 		b_scheme = scheme;
-*/
+	}
+	
 
-	/*while (true)
+	while (true)
 	{
 		for (gint i = 0; i < N; i++)
 		{
@@ -342,14 +354,37 @@ _benchmark_db_workload_insert(BenchmarkRun* run, JDBSchema* scheme, gchar const*
 			g_assert_true(ret);
 			g_assert_null(b_s_error);
 
+			if (!use_batch)
+			{
+				ret = j_batch_execute(batch);
+				g_assert_true(ret);
+			}
 			
 		}
 		
 		
+		if (use_batch || !use_timer)
+		{
 			ret = j_batch_execute(batch);
 			g_assert_true(ret);
+		}
 
+		if (use_timer && j_benchmark_iterate(run))
+		{
+			continue;
+		}
+		else
+		{
 			break;
-	}*/
+		}
+	}
 
+	// Reuse Insert function for other Benchmarks with use_timer flag
+	if (use_timer)
+	{
+		ret = j_batch_execute(delete_batch);
+		g_assert_true(ret);
+
+		run->operations = N;
+	}
 }
