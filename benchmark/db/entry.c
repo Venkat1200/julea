7458aa90057aa300d072f137da7d220709afc3cc
Venkat1200 <kvreddy1200@gmail.com>
2021-10-27 11:38:29 +0200

    Update entry.c

--- benchmark/db/entry.c
+++ benchmark/db/entry.c
@@ -368,9 +368,9 @@ _benchmark_db_workloadScientific(BenchmarkRun* run, gchar const* namespace, gboo
 static void
 benchmark_db_workloadScientific(BenchmarkRun* run)
 {
-	for(int i=0;i<100;i++){
-	_benchmark_db_insert(run, NULL, "benchmark_insert", false, false, false, true);
-	_benchmark_db_get_simple(run, "benchmark_get_simple", false, false);
+	for(int i=0;i<10000;i++){
+		_benchmark_db_insert(run, NULL, "benchmark_insert", false, false, false, true);
+		_benchmark_db_get_simple(run, "benchmark_get_simple", false, false);
 	}
 }
 /*
