ab556fd4d33d67ebf665922bad912e658ab993b0
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-11-15 23:53:30 +0100

    Update benchmark.c

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -390,10 +390,10 @@ main(int argc, char** argv)
 	j_benchmark_semantics = j_semantics_new_from_string(opt_template, opt_semantics);
 
 	benchmark_cache();
-/*	// Core
+	// Core
 	benchmark_background_operation();
 	benchmark_cache();
-	benchmark_memory_chunk();
+	/*benchmark_memory_chunk(); */
 	benchmark_message();
 
 	// KV client
@@ -401,7 +401,7 @@ main(int argc, char** argv)
 
 	// Object client
 	benchmark_distributed_object();
-	benchmark_object();    */
+	benchmark_object();   
 
 	// DB client
 	benchmark_db_entry();
