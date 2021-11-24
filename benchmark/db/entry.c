f7d7b1abda7a07c103263923eddc63399bd77f96
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-11-25 00:36:38 +0100

    Update entry.c

--- benchmark/db/entry.c
+++ benchmark/db/entry.c
@@ -1416,7 +1416,7 @@ static void benchmark_db_update_batch_index_mixed(BenchmarkRun *run) {
 
 void benchmark_db_entry(void) {
 
-/*	j_benchmark_add("/db/entry/insert", benchmark_db_insert);
+	j_benchmark_add("/db/entry/insert", benchmark_db_insert);
    
 	j_benchmark_add("/db/entry/insert-batch", benchmark_db_insert_batch);
 	j_benchmark_add("/db/entry/insert-index-single",
@@ -1431,7 +1431,7 @@ void benchmark_db_entry(void) {
 			benchmark_db_insert_index_mixed);
 	j_benchmark_add("/db/entry/insert-batch-index-mixed",
 			benchmark_db_insert_batch_index_mixed);
-/*	j_benchmark_add("/db/entry/delete", benchmark_db_delete);
+	j_benchmark_add("/db/entry/delete", benchmark_db_delete);
 	j_benchmark_add("/db/entry/delete-batch", benchmark_db_delete_batch);
 	j_benchmark_add("/db/entry/delete-index-single",
 			benchmark_db_delete_index_single);
@@ -1440,8 +1440,8 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/delete-index-all",
 			benchmark_db_delete_index_all);
 	j_benchmark_add("/db/entry/delete-batch-index-all",
-			benchmark_db_delete_batch_index_all);  */
-	/*	j_benchmark_add("/db/entry/workload 1(Scientific app)",
+			benchmark_db_delete_batch_index_all); 
+	j_benchmark_add("/db/entry/workload 1(Scientific app)",
 			benchmark_db_workloadScientific);
 	j_benchmark_add("/db/entry/workload Without index",
 			benchmark_db_workloadwithoutIndex);
@@ -1459,7 +1459,7 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/delete-index-mixed",
 			benchmark_db_delete_index_mixed);
 	j_benchmark_add("/db/entry/delete-batch-index-mixed",
-			benchmark_db_delete_batch_index_mixed); */
+			benchmark_db_delete_batch_index_mixed); 
 	j_benchmark_add("/db/entry/update", benchmark_db_update);
 	j_benchmark_add("/db/entry/update-batch", benchmark_db_update_batch);
 	j_benchmark_add("/db/entry/update-index-single",
