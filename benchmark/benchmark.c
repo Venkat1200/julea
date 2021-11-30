57b9b42f982c70d2b5d81f7e6586125c15d2f85b
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-11-30 23:19:41 +0100

    Update benchmark.c

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -395,19 +395,19 @@ main(int argc, char** argv)
 	benchmark_cache();
 	benchmark_memory_chunk(); 
 	benchmark_message();  */
-/*
+
 	// KV client
-	benchmark_kv(); */
+	benchmark_kv(); 
 	
    /*
 	// Object client
 	benchmark_distributed_object();
 	benchmark_object();   */
 
-	// DB client
+/*	// DB client
 	benchmark_db_entry();
 	benchmark_db_iterator();
-	benchmark_db_schema();  
+	benchmark_db_schema();  */
 	
 	
    /*	// Item client
