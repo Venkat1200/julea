0455aba53df88f41ec8e46f9e8bbe11625b1554b
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2021-12-30 14:24:46 +0100

    Update entry.c

--- benchmark/db/entry.c
+++ benchmark/db/entry.c
@@ -1695,7 +1695,7 @@ static void benchmark_db_update_batch_index_mixed(BenchmarkRun *run) {
 
 void benchmark_db_entry(void) {
 	
-/*	
+	
 	j_benchmark_add("/db/entry/insert", benchmark_db_insert);
 	j_benchmark_add("/db/entry/insert-batch", benchmark_db_insert_batch);
 	j_benchmark_add("/db/entry/insert-index-single",benchmark_db_insert_index_single);
@@ -1722,7 +1722,7 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/delete-index-all",benchmark_db_delete_index_all);
 	j_benchmark_add("/db/entry/delete-batch-index-all",benchmark_db_delete_batch_index_all);
 	j_benchmark_add("/db/entry/delete-index-mixed",benchmark_db_delete_index_mixed);                          
-	j_benchmark_add("/db/entry/delete-batch-index-mixed",benchmark_db_delete_batch_index_mixed);    */
+	j_benchmark_add("/db/entry/delete-batch-index-mixed",benchmark_db_delete_batch_index_mixed);    
 	
 	
 	
@@ -1732,7 +1732,7 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/workload 4(Autonomous Sys)",benchmark_db_workloadAutoSys);
 	j_benchmark_add("/db/entry/workload 5 (Write Intensive) ",benchmark_db_workload_Write_Intensive);     
 	
-/*	j_benchmark_add("/db/entry/workload Writes",benchmark_db_workload_Writes);     
+	j_benchmark_add("/db/entry/workload Writes",benchmark_db_workload_Writes);     
 	j_benchmark_add("/db/entry/workload Updates",benchmark_workload_Updates);    
 	
 	j_benchmark_add("/db/entry/workload Without index",benchmark_db_workloadwithoutIndex);
@@ -1742,6 +1742,6 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/workload With index All",benchmark_db_workloadwithIndexall);
 	
 	j_benchmark_add("/db/entry/workload Without Batch",benchmark_db_workloadwithoutBatch);
-	j_benchmark_add("/db/entry/workload With Batch",benchmark_db_workloadwithBatch);                    */
+	j_benchmark_add("/db/entry/workload With Batch",benchmark_db_workloadwithBatch);                  
 
 }
