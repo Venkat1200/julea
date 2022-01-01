67698809ef2585c59575446b12ead3496f92a89c
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2022-01-01 01:24:33 +0100

    Update benchmark.c

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -413,9 +413,8 @@ main(int argc, char** argv)
 	
 	
 	// DB client
-       
+        benchmark_db_schema();        
 	benchmark_db_entry();
-	benchmark_db_schema(); 
 	benchmark_db_iterator();
 	
 	
