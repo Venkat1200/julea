134470d0384ce39d8c5bc6af26afdbb0f7a57d60
Venkat1200 <38086568+Venkat1200@users.noreply.github.com>
2022-01-11 00:31:01 +0100

    Update entry.c

--- benchmark/db/entry.c
+++ benchmark/db/entry.c
@@ -1697,36 +1697,36 @@ void benchmark_db_entry(void) {
 	
 	
 	j_benchmark_add("/db/entry/insert", benchmark_db_insert);
-	j_benchmark_add("/db/entry/insert-batch", benchmark_db_insert_batch);
+/*	j_benchmark_add("/db/entry/insert-batch", benchmark_db_insert_batch);
 	j_benchmark_add("/db/entry/insert-index-single",benchmark_db_insert_index_single);
 	j_benchmark_add("/db/entry/insert-batch-index-single",benchmark_db_insert_batch_index_single);
 	j_benchmark_add("/db/entry/insert-index-all",benchmark_db_insert_index_all);
 	j_benchmark_add("/db/entry/insert-batch-index-all",benchmark_db_insert_batch_index_all);
 	j_benchmark_add("/db/entry/insert-index-mixed",benchmark_db_insert_index_mixed);
-	j_benchmark_add("/db/entry/insert-batch-index-mixed",benchmark_db_insert_batch_index_mixed);
+	j_benchmark_add("/db/entry/insert-batch-index-mixed",benchmark_db_insert_batch_index_mixed);  */
 	
 	j_benchmark_add("/db/entry/update", benchmark_db_update);
-	j_benchmark_add("/db/entry/update-batch", benchmark_db_update_batch);
+/*	j_benchmark_add("/db/entry/update-batch", benchmark_db_update_batch);
 	j_benchmark_add("/db/entry/update-index-single",benchmark_db_update_index_single);
 	j_benchmark_add("/db/entry/update-batch-index-single",benchmark_db_update_batch_index_single);
 	j_benchmark_add("/db/entry/update-index-all",benchmark_db_update_index_all);
 	j_benchmark_add("/db/entry/update-batch-index-all",benchmark_db_update_batch_index_all);
 	j_benchmark_add("/db/entry/update-index-mixed",benchmark_db_update_index_mixed);
-	j_benchmark_add("/db/entry/update-batch-index-mixed",benchmark_db_update_batch_index_mixed);  
+	j_benchmark_add("/db/entry/update-batch-index-mixed",benchmark_db_update_batch_index_mixed);  */
 	
 	
 	j_benchmark_add("/db/entry/delete", benchmark_db_delete);
-	j_benchmark_add("/db/entry/delete-batch", benchmark_db_delete_batch);
+/*	j_benchmark_add("/db/entry/delete-batch", benchmark_db_delete_batch);
 	j_benchmark_add("/db/entry/delete-index-single",benchmark_db_delete_index_single);
 	j_benchmark_add("/db/entry/delete-batch-index-single",benchmark_db_delete_batch_index_single);
 	j_benchmark_add("/db/entry/delete-index-all",benchmark_db_delete_index_all);
 	j_benchmark_add("/db/entry/delete-batch-index-all",benchmark_db_delete_batch_index_all);
 	j_benchmark_add("/db/entry/delete-index-mixed",benchmark_db_delete_index_mixed);                          
-	j_benchmark_add("/db/entry/delete-batch-index-mixed",benchmark_db_delete_batch_index_mixed);    
+	j_benchmark_add("/db/entry/delete-batch-index-mixed",benchmark_db_delete_batch_index_mixed);    */
 	
 	
 	
-	j_benchmark_add("/db/entry/workload 1(Scientific app)",benchmark_db_workloadScientific);      
+/*	j_benchmark_add("/db/entry/workload 1(Scientific app)",benchmark_db_workloadScientific);      
 	j_benchmark_add("/db/entry/workload 2(Streaming)",benchmark_db_workloadStreaming);
 	j_benchmark_add("/db/entry/workload 3(Machine Learning)",benchmark_db_workloadML);
 	j_benchmark_add("/db/entry/workload 4(Autonomous Sys)",benchmark_db_workloadAutoSys);
@@ -1742,6 +1742,6 @@ void benchmark_db_entry(void) {
 	j_benchmark_add("/db/entry/workload With index All",benchmark_db_workloadwithIndexall);
 	
 	j_benchmark_add("/db/entry/workload Without Batch",benchmark_db_workloadwithoutBatch);
-	j_benchmark_add("/db/entry/workload With Batch",benchmark_db_workloadwithBatch);                  
+	j_benchmark_add("/db/entry/workload With Batch",benchmark_db_workloadwithBatch);         /:         
 
 }
